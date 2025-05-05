cedd72592849370db364509f2287892665f27e49
martin <martin.neron@laposte.net>
2025-05-05 17:20:23 +0200

    init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+    public static void main(String... args) {
+        System.out.println("hello world");
+    }
+}
