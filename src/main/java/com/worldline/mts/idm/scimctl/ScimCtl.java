b91fe5f47efcbe1f404b7e9b3b6546d37af951b9
martin <martin.neron@laposte.net>
2025-06-17 10:30:15 +0200

    fix case sensitive bug on resource type

--- src/main/java/com/worldline/mts/idm/scimctl/ScimCtl.java
+++ src/main/java/com/worldline/mts/idm/scimctl/ScimCtl.java
@@ -5,8 +5,12 @@ import com.worldline.mts.idm.scimctl.commands.get_cmd.GetCommand;
 
 import com.worldline.mts.idm.scimctl.commands.create_cmd.CreateCommand;
 import com.worldline.mts.idm.scimctl.commands.import_cmd.ImportCommand;
+
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import io.quarkus.oidc.client.OidcClientException;
+import io.quarkus.oidc.common.runtime.OidcCommonUtils;
+import io.quarkus.runtime.Quarkus;
 import jakarta.inject.Inject;
 
 import picocli.CommandLine;
@@ -17,8 +21,7 @@ import com.worldline.mts.idm.scimctl.commands.update_cmd.UpdateCommand;
 @QuarkusMain
 @CommandLine.Command(name = "scim-ctl", mixinStandardHelpOptions = true, subcommands = { ScimSchema.class,
     GetCommand.class, CreateCommand.class, ImportCommand.class, UpdateCommand.class, DeleteCommand.class })
-public class ScimCtl implements QuarkusApplication {
-
+public class ScimCtl implements QuarkusApplication{
   @Inject
   CommandLine.IFactory factory;
 
@@ -26,4 +29,5 @@ public class ScimCtl implements QuarkusApplication {
   public int run(String... args) {
     return new CommandLine(this, factory).execute(args);
   }
+ 
 }
--- src/main/java/com/worldline/mts/idm/scimctl/commands/common/ResourceTypeConverter.java
+++ src/main/java/com/worldline/mts/idm/scimctl/commands/common/ResourceTypeConverter.java
@@ -1,14 +1,26 @@
 package com.worldline.mts.idm.scimctl.commands.common;
 
+import org.antlr.v4.parse.ANTLRParser.throwsSpec_return;
+
 import picocli.CommandLine;
 
 public class ResourceTypeConverter implements CommandLine.ITypeConverter<FilterCommonOptions.ResourceType> {
-    @Override
-    public FilterCommonOptions.ResourceType convert(String value) {
-        try {
-            return FilterCommonOptions.ResourceType.valueOf(value.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new CommandLine.TypeConversionException("Invalid resource type must be 'USER' or 'GROUP' (case insensitive) : " + value);
-        }
+
+  @Override
+  public FilterCommonOptions.ResourceType convert(String value) throws CommandLine.ParameterException {
+    try {
+      return valueToResourceType(value);
+    } catch (IllegalArgumentException e) {
+     throw new CommandLine.ParameterException(new CommandLine(this.getClass()), e.getMessage());
     }
+  }
+
+  private FilterCommonOptions.ResourceType valueToResourceType(String value) throws IllegalArgumentException {
+    value = value.toLowerCase();
+    if (value.equals("user"))
+      return FilterCommonOptions.ResourceType.USER;
+    if (value.equals("group"))
+      return FilterCommonOptions.ResourceType.GROUP;
+    throw new IllegalArgumentException("Invalid resource type must be 'user' or 'group' (case insensitive) : " + value);
+  }
 }
